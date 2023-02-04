afe2221105b35e60f742b3c255a1f56143536a84
ilfa <ilfahassan@gmail.com>
2023-02-04 13:24:37 +0000

    Number of Beings component added to input number of beings into the form

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import W12MHeader from './W12MHeader';
 import SpeciesName from './speciesName';
 import PlanetName from './PlanetName';
+import NumberOfBeings from './NumberOfBeings';
 
 
 
@@ -9,11 +10,13 @@ import PlanetName from './PlanetName';
 const W12MForm = () => {
 	const [speciesName, setSpeciesName] = useState<string>('humans');
 	const[planetName,setPlanetname]=useState<string>('Earth');
+	const[numberOfBeings,setNumberOfBeings]=useState<number>(100);
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
 			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(e : any) => setSpeciesName(e.target.value)} />
 			<PlanetName planetName={planetName} onChangePlanetName={(e : any) => setPlanetname(e.target.value)} />
+			<NumberOfBeings numberOfBeings={numberOfBeings} onChangeNumberOfBeings={(e : any) => setNumberOfBeings(e.target.value)} />
 			{/* REST OF FORM GOES HERE */}
 		</section>
 	);
