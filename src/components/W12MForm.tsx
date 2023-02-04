91f6b40f6d57b13e8d459eb48b5823e48278ce34
ilfa <ilfahassan@gmail.com>
2023-02-04 13:11:05 +0000

    Added another component to render planet name onto the screen

--- src/components/W12MForm.tsx
+++ src/components/W12MForm.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react';
 import W12MHeader from './W12MHeader';
 import SpeciesName from './speciesName';
+import PlanetName from './PlanetName';
 
 
 
 
 const W12MForm = () => {
 	const [speciesName, setSpeciesName] = useState<string>('humans');
+	const[planetName,setPlanetname]=useState<string>('Earth');
 	return (
 		<section className='w12MForm'>
 			<W12MHeader />
 			<SpeciesName speciesName={speciesName} onChangeSpeciesName={(e : any) => setSpeciesName(e.target.value)} />
+			<PlanetName planetName={planetName} onChangePlanetName={(e : any) => setPlanetname(e.target.value)} />
 			{/* REST OF FORM GOES HERE */}
 		</section>
 	);
